172989fe6dc117bd7945d52b74c12215c236d5c9
nguivo <nguivo9@gmail.com>
2024-05-29 22:06:53 +0100

    .env and .env.example files created and dotenv implemented

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ use framework\core\Application;
 
 require_once dirname(__DIR__)."/vendor/autoload.php";
 
+$dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 $config = [];
 
 $app = new Application(dirname(__DIR__), $config);

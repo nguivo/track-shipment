99558d52a7f0f5cde7ae4fc9d4b4e935dea24f26
nguivo <nguivo9@gmail.com>
2024-05-29 20:57:51 +0100

    namespaces adjusted. About to start building seriously

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-    echo "This is the entry point";
+use framework\core\Application;
+
+require_once dirname(__DIR__)."/vendor/autoload.php";
+
+$config = [];
+
+$app = new Application(dirname(__DIR__), $config);
+
+echo "This is the entry point";
